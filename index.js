2bb394d397b87f36e2e6b54b1f2146ae0dc1f61d
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-12 20:59:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ class Route{
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
     let shb= eastWest.indexOf(this.beginningLocation.horizontal)
     let ehb = eastWest.indexOf(this.endingLocation.horizontal)
-    let b = d
+    let b = d()
     
     debugger
     let horzBlocks = "h"
