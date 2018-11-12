a677291143d60e27cd906229457cf2a485de0e56
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-12 20:43:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class Route{
   
   blocksTravelled(){
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-    
+    let horBlocks = 
   }
   
 }
